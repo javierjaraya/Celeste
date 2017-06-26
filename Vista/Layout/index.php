dd9fc8445d9140225ebfce8d04fb2b3dc1a685ef
javie <javie@Notebook-Javier>
2017-06-26 13:29:48 -0300

    Actualizar Carusel

--- Vista/Layout/index.php
+++ Vista/Layout/index.php
@@ -40,12 +40,12 @@ if (isset($_SESSION["autentificado"])) {
         <script type="text/javascript" charset="utf8" src="../../Files/Complementos/datatables/jquery.dataTables.js"></script>
         <script type="text/javascript" src="../../Files/Complementos/jquery-easyui-1.4.2/jquery.easyui.min.js"></script>
         <!--<script type="text/javascript" src="../../Files/js/jquery-1.9.1.min.js"></script>-->
-        <script type="text/javascript" src="../../Files/js/jquery-1.7.1.min.js"></script>
+        <!--<script type="text/javascript" src="../../Files/js/jquery-1.7.1.min.js"></script>-->
         <script type="text/javascript" src="../../Files/Complementos/menuDespegable/js-menu.js"></script>
-        
+
         <script type="text/javascript" src="../../Files/js/jquery.nivo.slider.pack.js"></script>
         <script type="text/javascript" src="../../Files/js/jquery.jcarousel.min.js"></script>
-        
+
         <!-- JS Part End-->
 
         <!-- Bootstrap 3.3.6 -->
@@ -131,12 +131,55 @@ if (isset($_SESSION["autentificado"])) {
                             <!--Middle Part Start-->
                             <div id="content" style="padding-top: 0">
                                 <!--Slideshow Part Start-->
-                                <div class="slider-wrapper">
-                                    <div id="slideshow" class="nivoSlider"> <a href="#"><img src="../../Files/img/slider/slider-1.jpg" alt="slideshow-1" /></a> <a href="#"><img src="../../Files/img/slider/slider-2.jpg" alt="slideshow-2" /></a> <a href="#"><img src="../../Files/img/slider/slider-3.jpg" alt="slideshow-3" /></a> <a href="#"><img src="../../Files/img/slider/slider-4.jpg" alt="slideshow-4" /></a> </div>
+                                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                                    <!-- Indicators -->
+                                    <ol class="carousel-indicators">
+                                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                                    </ol>
+
+                                    <!-- Wrapper for slides -->
+                                    <div class="carousel-inner" role="listbox">
+                                        <div class="item active">
+                                            <img src="../../Files/img/slider/slider-1.jpg" alt="...">
+                                            <div class="carousel-caption">
+                                                <!-- Texto descripcion aqui -->
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <img src="../../Files/img/slider/slider-2.jpg" alt="...">
+                                            <div class="carousel-caption">
+                                                <!-- Texto descripcion aqui -->
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <img src="../../Files/img/slider/slider-3.jpg" alt="...">
+                                            <div class="carousel-caption">
+                                                <!-- Texto descripcion aqui -->
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <img src="../../Files/img/slider/slider-4.jpg" alt="...">
+                                            <div class="carousel-caption">
+                                                <!-- Texto descripcion aqui -->
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <!-- Controls -->
+                                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                        <span class="sr-only">Anterior</span>
+                                    </a>
+                                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                        <span class="sr-only">Siguiente</span>
+                                    </a>
                                 </div>
                                 <script type="text/javascript">
                                     $(document).ready(function () {
-                                        $('#slideshow').nivoSlider();
+                                        $('.carousel').carousel();
                                     });
                                 </script>
                                 <!--Slideshow Part End-->
