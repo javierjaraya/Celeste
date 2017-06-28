bea7285b03c5c5942e606433977bf5d239c95fdf
Joseline Cisternas <Joseline Cisternas@Joseline>
2017-06-28 03:07:04 -0400

    quienes somos

--- Vista/Menus/menuCliente.php
+++ Vista/Menus/menuCliente.php
@@ -1,5 +1,6 @@
 <ul>
-    <li class="home"><a title="Home" href="index.php"><span>Inicio</span></a></li>    
+    <li class="home"><a title="Home" href="index.php"><span>Inicio</span></a></li>  
+    <li><a href="quienesSomos.php">Quienes Somos</a></li>
     <li><a href="carroDeCompra.php">CarroDeCompra</a></li>
     <li><a href="miHistorialDeCompras.php">Mis Compras</a></li>
     <li><a href="miPerfil.php">Mi Perfil</a></li>
--- Vista/Menus/menuVisitante.php
+++ Vista/Menus/menuVisitante.php
@@ -1,5 +1,6 @@
 <ul>
     <li class="home"><a title="Home" href="index.php"><span>Inicio</span></a></li>
+    <li><a href="quienesSomos.php">Quienes Somos</a></li>
     <li><a href="registrarUsuario.php">Registrarse</a></li>
     <li><a href="iniciarSesion.php">Iniciar Sesion</a></li>
 </ul>
